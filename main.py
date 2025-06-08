e0f30dd09056b7812a3e4711be03c6b4dbbfc8fb
kirillmiro <kirill.miro2000@gmail.com>
2025-06-08 13:39:41 +0300

    kirill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot 
 from config import token
-
 from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
@@ -14,6 +13,29 @@ def go(message):
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
 
+
+@bot.message_handler(commands=['feed'])
+def feed(message):
+    username = message.from_user.username
+    if username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[username]
+        pokemon.base_experience += 1
+        bot.reply_to(message, f"{pokemon.name} покормлен! Опыт теперь: {pokemon.base_experience}")
+    else:
+        bot.reply_to(message, "Сначала создай покемона командой /go")
+
+        
 bot.infinity_polling(none_stop=True)
-
